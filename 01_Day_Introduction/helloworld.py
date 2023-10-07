905672c9ae91d208fc5715eb8c2b6ff9263fe931
Saurav Kumar <ksksaurav@gmail.com>
2023-10-08 01:00:05 +0530

    Want to test datatypes

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -11,11 +11,15 @@ print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+# print(type(10))                  # Int
+# print(type(3.14))                # Float
+# print(type(1 + 3j))              # Complex
+# print(type('Asabeneh'))          # String
+# print(type([1, 2, 3]))           # List
+# print(type({'name':'Asabeneh'})) # Dictionary
+# print(type({9.8, 3.14, 2.7}))    # Set
+# print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+print(type(10))
+print(type(3.14))
